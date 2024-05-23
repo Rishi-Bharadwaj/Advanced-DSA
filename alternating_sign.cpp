8a52ca65dfa082718295dbc136630a17362c959d
Rishi-Bharadwaj <154824463+Rishi-Bharadwaj@users.noreply.github.com>
2024-05-23 21:20:05 +0530

    Update alternating_sign.cpp

--- alternating_sign.cpp
+++ alternating_sign.cpp
@@ -1,8 +1,10 @@
+//There’s an array ‘A’ of size ‘N’ with an equal number of positive and negative elements.
+//Without altering the relative order of positive and negative elements, you must return
+//an array of alternately positive and negative values.
+//O(2N)
 #include<iostream>
 
 using namespace std;
-
-//O(2N)
 
 int main()
 {
